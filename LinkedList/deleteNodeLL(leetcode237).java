1c531e8ba5315ef084e254c0124eb6912a12997d
Vidushi Haksar <60004269+vidushihaksar@users.noreply.github.com>
2020-06-11 00:41:27 +0530

    first commit

---
+++ LinkedList/deleteNodeLL(leetcode237).java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev=null;
+        while(node.next!=null){
+            node.val = node.next.val;
+            
+            if(node.next.next == null){
+                prev =node;
+            }
+            node =node.next;
+        }
+        prev.next = null;
+    }
+}
