a4fa39e372fe470085563c384429db1440cfb60c
Vidushi Haksar <60004269+vidushihaksar@users.noreply.github.com>
2020-07-03 13:02:54 +0530

    first commit

---
+++ Graph/isCyclic.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+        System.out.println(isCyclic(graph));
+
+   }
+   
+   public static boolean isCyclic(ArrayList<Edge>[] graph){
+       
+           boolean visited[] = new boolean[graph.length];
+       
+       for(int v = 0; v< graph.length; v++){
+           
+           if(!visited[v]){
+               
+               if(bfs(v, graph,visited)){
+                   //******* graph is cyclic ********
+                   return true;
+               }
+           }
+           
+       }
+       //******* graph is not cyclic ********
+       return false;
+   }
+   
+   
+   public static boolean bfs(int src, ArrayList<Edge>[] graph, boolean[] visited){
+       
+       Queue<Integer> que = new ArrayDeque<>();
+       que.add(src);
+
+       while(que.size() > 0){
+           //remove;
+           int vtx = que.remove();
+
+           //*** componenet is cyclic ******
+           if( visited[vtx] ){
+           
+               return true;
+               
+            //*** componenet is not cyclic ******
+           }else{
+               //mark
+               visited[vtx] = true;
+               
+               //add all unvisited neighbours
+               for(Edge e : graph[vtx]){
+                     if(!visited[e.nbr]){
+                        que.add(e.nbr);
+                     }
+               }
+           }
+       }
+       return false;
+   }
+}
