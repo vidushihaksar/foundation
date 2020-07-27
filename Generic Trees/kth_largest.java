ddc1f9b9ca3d931cefd33fa1aaf17ba0103aeb04
Vidushi Haksar <60004269+vidushihaksar@users.noreply.github.com>
2020-07-27 23:39:27 +0530

    first commit

---
+++ Generic Trees/kth_largest.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  
+  
+  static int floor;
+  public static void ceilAndFloor(Node node, int data) {
+     
+           //System.out.println(data);
+
+    if(node.data < data){
+        floor = Math.max(floor, node.data); 
+     }
+     for(Node n : node.children){
+         ceilAndFloor(n, data);
+     }
+  }
+
+  public static int kthLargest(Node node, int k){
+    
+    int factor = Integer.MAX_VALUE;
+    
+    for(int i = 1; i <= k; i++){
+        ceilAndFloor(node , factor);
+        factor = floor;
+        floor= Integer.MIN_VALUE;
+    }
+    return factor;
+    
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+    floor = Integer.MIN_VALUE;
+    int k = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int kthLargest = kthLargest(root, k);
+    
+    System.out.println(kthLargest);
+  }
+
+}
